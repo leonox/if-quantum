653d45328168b271f7c571d7f1c5fc7dd74c957e
tim.leonhardt <tim.leonhardt@accenture.com>
2019-11-28 18:18:25 +0100

    no message

--- setup.py
+++ setup.py
@@ -3,16 +3,10 @@ from distutils.core import setup
 setup(
     name='ifquantum',
     version='0.0.1',
-    author='bosoko-leonox',
-    #author_email='jrh@example.com',
-    #packages=['towelstuff', 'towelstuff.test'],
-    #scripts=['bin/stowe-towels.py','bin/wash-towels.py'],
-    #url='http://pypi.python.org/pypi/TowelStuff/',
-    #license='LICENSE.txt',
-    description='Visualize Quantum Entanglement',
-    long_description=open('README.md').read(),
-    #install_requires=[
-    #    "Django >= 1.1.1",
-    #    "caldav == 0.1.4",
-    #],
+    packages=['if_quantum'],
+    url='https://github.com/leonox/if-quantum',
+    license='-',
+    author='tim.leonhardt',
+    author_email='-',
+    description='vizualize quantum entanglement'
 )
